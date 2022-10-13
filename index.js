42fb3f708c687248aa29c786c7d95995afc21aa4
alfredmjz <alfredmjz@gmail.com>
2022-10-12 20:22:54 -0400

    add user authorization

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const { ApolloServer, UserInputError, gql } = require("apollo-server");
+const { ApolloServer, UserInputError, AuthenticationError, gql } = require("apollo-server");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
 const Author = require("./models/author");
 const Book = require("./models/book");
+const User = require("./models/user");
+
+const JWT_SECRET = "NEED_HERE_A_SECRET_KEY";
 
 let authors = [
 	{
@@ -124,9 +129,21 @@ const typeDefs = gql`
 		id: ID!
 	}
 
+	type User {
+		username: String!
+		favouriteGenre: String!
+		id: ID!
+	}
+
+	type Token {
+		value: String!
+	}
+
 	type Mutation {
 		addBook(title: String!, published: Int!, author: String!, genres: [String!]!): Book!
 		editAuthor(name: String!, setBornTo: Int!): Author
+		createUser(username: String!, favouriteGenre: String!): User
+		login(username: String!, password: String!): Token
 	}
 
 	type Query {
@@ -134,6 +151,7 @@ const typeDefs = gql`
 		authorCount: Int!
 		allBooks(author: String, genre: String): [Book!]
 		allAuthors: [Author!]!
+		me: User
 	}
 `;
 
@@ -172,6 +190,10 @@ const resolvers = {
 			const response = await Author.find({});
 			return response;
 		},
+		me: (root, args, context) => {
+			console.log(context);
+			return context.currentUser;
+		},
 	},
 	Author: {
 		bookCount: async (root) => {
@@ -186,7 +208,12 @@ const resolvers = {
 		},
 	},
 	Mutation: {
-		addBook: async (root, args) => {
+		addBook: async (root, args, context) => {
+			const currentUser = context.currentUser;
+			if (!currentUser) {
+				throw new AuthenticationError("not authenticated");
+			}
+
 			if (args.name.length <= 3) {
 				throw new UserInputError("Author name must be more than 3 characters", {
 					invalidArgs: args.name,
@@ -196,6 +223,7 @@ const resolvers = {
 					invalidArgs: args.setBornTo,
 				});
 			}
+
 			const exist = await Author.findOne({ name: args.author });
 			try {
 				if (!exist) {
@@ -214,7 +242,12 @@ const resolvers = {
 				});
 			}
 		},
-		editAuthor: async (root, args) => {
+		editAuthor: async (root, args, context) => {
+			const currentUser = context.currentUser;
+			if (!currentUser) {
+				throw new AuthenticationError("not authenticated");
+			}
+
 			if (args.name.length <= 3) {
 				throw new UserInputError("Author name must be more than 3 characters", {
 					invalidArgs: args.name,
@@ -239,12 +272,42 @@ const resolvers = {
 				});
 			}
 		},
+		createUser: async (root, args) => {
+			const user = new User({ ...args });
+
+			return user.save().catch((error) => {
+				throw new UserInputError(error.message, {
+					invalidArgs: args,
+				});
+			});
+		},
+		login: async (root, args) => {
+			const user = await User.findOne({ username: args.username });
+			if (!user || args.password !== "secret") {
+				throw new UserInputError("wrong credentials");
+			}
+
+			const userForToken = {
+				username: user.username,
+				id: user._id,
+			};
+
+			return { value: jwt.sign(userForToken, JWT_SECRET) };
+		},
 	},
 };
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
+	context: async ({ req }) => {
+		const auth = req ? req.headers.authorization : null;
+		if (auth && auth.toLowerCase().startsWith("bearer ")) {
+			const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
+			const currentUser = await User.findById(decodedToken.id).populate("favouriteGenre");
+			return { currentUser };
+		}
+	},
 });
 
 server.listen().then(({ url }) => {
